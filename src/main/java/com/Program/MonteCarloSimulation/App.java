c09216b3a48390d081860cbc3b9e63eff6e7d708
Ankur Patil <asp549@nyu.edu>
2017-06-21 12:49:44 -0700

    indent file

--- src/main/java/com/Program/MonteCarloSimulation/App.java
+++ src/main/java/com/Program/MonteCarloSimulation/App.java
@@ -13,24 +13,23 @@ public class App {
     	simulationResult = simulator.calculateConservativeSimulation();
     	calculateConservativeFinding(simulationResult);
     }
-
-	private static void calculateAggessiveFinding(double[] simulationResult) {
+	
+    private static void calculateAggessiveFinding(double[] simulationResult) {
 		Calculate calculate = new Calculate();
 		double aggressiveMedian = calculate.findMedian(simulationResult);
         double aggressiveTop10Percentile = calculate.findTop10Percentile(simulationResult);
         double aggressiveBottom10Percentile =calculate.findBottom10Percentile(simulationResult);
         System.out.printf("%n Aggressive Median     Top 10 Percentile   Bottom 10 Percentile");
         System.out.printf("%n       %.2f         %.2f           %.2f ",aggressiveMedian,aggressiveTop10Percentile,aggressiveBottom10Percentile);		
-	}
+    }
 	
-	private static void calculateConservativeFinding(double[] simulationResult) {
+    private static void calculateConservativeFinding(double[] simulationResult) {
 		Calculate calculate = new Calculate();
 		double conservativeMedian = calculate.findMedian(simulationResult);
         double conservativeTop10Percentile = calculate.findTop10Percentile(simulationResult);
         double conservativeBottom10Percentile =calculate.findBottom10Percentile(simulationResult);
         System.out.printf("%n Conservative Median   Top 10 Percentile   Bottom 10 Percentile");
         System.out.printf("%n       %.2f         %.2f           %.2f ",conservativeMedian,conservativeTop10Percentile,conservativeBottom10Percentile);
-		
 	}
 
     
